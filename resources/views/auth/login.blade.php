f72b607f09d8c59235dd16218cc26fe08d6046b6
THANH <2169878@gmail.com>
2016-10-14 11:44:31 +1100

    modified login and register templates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,8 +5,20 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+                <div class="panel-heading text-center">
+                    <h4>Login Form</h4>
+                </div>
                 <div class="panel-body">
+                    @if (count($errors) > 0)
+                        <p>error</p>
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
 
