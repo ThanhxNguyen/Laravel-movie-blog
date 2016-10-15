19c23d98fb9ca2b69927de45a9a303c142f57033
rmit-s3499308-yifan-yao <s3499308@student.rmit.edu.au>
2016-10-16 10:32:02 +1100

    unit testing update

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -11,9 +11,40 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicExample()
+
+    public function testCantViewWishListBeforeLogin()
     {
         $this->visit('/')
-             ->see('Laravel 5');
+             ->click('Wish List')
+             ->seePageIs("/login");
     }
+
+    public function testMoviesActuallyStoreInDatabase()
+    {
+        $this->seeInDatabase('movies', ['title' => 'The Hollow']);
+    }
+
+    public function testUserHasOwnSession()
+    {
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user)
+            ->withSession(['foo' => 'bar'])
+            ->visit('/');
+    }
+
+    /*
+    public function testEachUserHasSufficientInformation()
+    {
+        // Assert that each user in the list has at least an id, name and email attribute.
+        $this->get('/users')
+            ->seeJsonStructure([
+                '*' => [
+                    'id', 'name', 'email'
+                ]
+            ]);
+    }
+    */
+
+
 }
